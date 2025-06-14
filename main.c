ab6057a328ed04b73f0b415c5a62f4b2aa5445a9
Jhonatan Torres <jhonatan_pts144@hotmail.com>
2025-06-14 12:30:35 -0500

    Agrega plantilla main.c inical

---
+++ main.c
@@ -0,0 +1,19 @@
+/*
+ * Proyecto: Ejecución de Múltiples Tareas
+ * Autores: Jhonatan Torres, Sabina Zabala, Sebastían Yánes y Geovanni 
+ * Fecha: 17 de junio 2025
+ *
+ * Versión inicial: stub de main para integración con Git.
+ */
+
+#include <stdio.h>
+
+int main(void) {
+    // Aquí implementaremos:
+    // - Versión con fork()
+    // - Versión con pthread
+    // Para empezar, solo imprimimos un mensaje de bienvenida.
+
+    printf("Proyecto SO: Procesos vs Hilos\n");
+    return 0;
+}
